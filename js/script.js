d64ca24ae351b66f9d20ce0a24cabd11fc8737b6
Amarildo <amarildos@outloo.it>
2024-01-08 14:45:30 +0100

    added file in html, created a style for list

--- js/script.js
+++ js/script.js
@@ -11,4 +11,20 @@ console.log('JS OK');
 // 1. Creo una lista della spesa;
 
 const listaSpesa = ['detersivi', 'pane', 'latte', 'frutta', 'verdura'];
+// 2. stampo sulla pagina
 console.log(listaSpesa);
+
+// 4. prendo l'id dell'ul
+const spesa = document.querySelector('ul');
+
+// 5. inizzializo il ciclo while
+let articoli = 0;
+while (articoli < listaSpesa.length){
+    // 6. creo gli elementi all'interno dell'ul
+    const listItem = document.createElement('li');
+      listItem.textContent = listaSpesa[articoli];
+
+      spesa.appendChild(listItem);
+
+      articoli++;
+}
