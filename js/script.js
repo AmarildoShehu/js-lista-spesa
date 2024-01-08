f16473a01c3ebc142c1a7716d0128c00597e377f
Amarildo <amarildos@outloo.it>
2024-01-08 14:20:31 +0100

    Entred my set list

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console.log('JS OK');
+
+// 1. Creo una lista della spesa;
+// 2. stampo sulla pagina
+// 3. creo in html ul
+// 4. prendo l'id dell'ul
+// 5. inizzializo il ciclo while
+// 6. creo gli elementi all'interno dell'ul
