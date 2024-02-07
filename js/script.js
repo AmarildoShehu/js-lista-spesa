e5f736a73ffad5c54d12eb23eb7724248f19d686
Amarildo <you@example.com>
2024-02-07 16:13:52 +0100

    fix prova

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log('JS OK');
+console.log("JS OK");
 
 // 1. Creo una lista della spesa;
 // 2. stampo sulla pagina
@@ -7,24 +7,25 @@ console.log('JS OK');
 // 5. inizzializo il ciclo while
 // 6. creo gli elementi all'interno dell'ul
 
-
 // 1. Creo una lista della spesa;
 
-const listaSpesa = ['detersivi', 'pane', 'latte', 'frutta', 'verdura'];
+const listaSpesa = ["detersivi", "pane", "latte", "frutta", "verdura"];
 // 2. stampo sulla pagina
 console.log(listaSpesa);
 
 // 4. prendo l'id dell'ul
-const spesa = document.querySelector('ul');
+const spesa = document.querySelector("ul");
 
 // 5. inizzializo il ciclo while
 let articoli = 0;
-while (articoli < listaSpesa.length){
-    // 6. creo gli elementi all'interno dell'ul
-    const listItem = document.createElement('li');
-      listItem.textContent = listaSpesa[articoli];
+while (articoli < listaSpesa.length) {
+  // 6. creo gli elementi all'interno dell'ul
+  const listItem = document.createElement("li");
+  listItem.textContent = listaSpesa[articoli];
 
-      spesa.appendChild(listItem);
+  spesa.appendChild(listItem);
 
-      articoli++;
+  articoli++;
 }
+
+// ciao
